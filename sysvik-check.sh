5f803c6fd6e33d4f538f8237746f81b8281c1272
Tryggvi Farestveit <tryggvi@linux.is>
2020-12-30 17:11:48 +0000

    Added check for invalid rpm, systemd support etc

--- sysvik-check.sh
+++ sysvik-check.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #
-# Version: 20081008-01
-# Monitors if sysvik-data is running and if not, tries to start it
+# Version: 20201229-01
+# 
+# This script performs housekeeping stuff for Sysvik
 # 
 # This script is part of sysvik package.
 ###################################################################
@@ -14,13 +15,31 @@ then
 	echo "Everything is fine" > /dev/null
 else
 	echo "$NOW Sysvik-check: sysvik-data not running, starting it up" >> /var/log/sysvik.log
-        if [ -f /etc/init.d/sysvikd ]
+	if [ -e /usr/bin/systemctl ]; then
+		if [ ! -e /usr/lib/systemd/system//sysvik-data.service ]; then
+			echo "$NOW Systemctl script for Sysvik missing. Creating" >> /var/log/sysvik.log
+			# Systemctl file missing
+			cp -f /var/lib/sysvik/sysvik-data.service /usr/lib/systemd/system//sysvik-data.service
+			/usr/bin/systemctl daemon-reload
+			/usr/bin/systemctl start sysvik-data.service
+			/usr/bin/systemctl enable sysvik-data.service
+			sleep 5
+			/usr/bin/systemctl restart sysvik-data.service
+		fi
+	elif [ -f /etc/init.d/sysvikd ]
         then
+		echo "$NOW Starting via initd" >> /var/log/sysvik-housekeeping.log
                 /etc/init.d/sysvikd start
         fi
+fi
 
-        if [ ! -f /etc/init.d/sysvikd ]
-        then
-                /usr/sbin/sysvik-data -b -q 
-        fi
+# Check	for invalid version and	uninstall sysvik-3.2r3-4.noarch
+if [[ -e /usr/bin/rpm && ! -e /var/lib/sysvik/sysvik-3.2r3-4.fix ]]; then
+	if /usr/bin/rpm -q sysvik-3.2r3-4.noarch; then
+		echo "$NOW Package sysvik-3.2r3-4.noarch installed, removing" >> /var/log/sysvik.log
+		/usr/bin/rpm -e --justdb --noscripts sysvik-3.2r3-4.noarch
+		/usr/bin/touch /var/lib/sysvik/sysvik-3.2r3-4.fix
+	else
+		/usr/bin/touch /var/lib/sysvik/sysvik-3.2r3-4.fix
+	fi
 fi
